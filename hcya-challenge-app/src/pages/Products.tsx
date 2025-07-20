03d76e775b6b536042b9ded0e9dd545a1ed19f77
diego <die_ore@hotmail.com>
2025-07-20 16:33:12 -0300

    feat: add error handling and show products

--- hcya-challenge-app/src/pages/Products.tsx
+++ hcya-challenge-app/src/pages/Products.tsx
@@ -1,8 +1,29 @@
 import { Typography, Box, Button, TextField } from "@mui/material";
 import { useState } from "react";
+import { useDebounce } from "../hooks/useDebounce";
+import { useProducts } from "../hooks/products";
+import type { Product } from "../services/productService";
 
 export default function Products() {
   const [search, setSearch] = useState("");
+  const debouncedSearch = useDebounce(search, 500);
+
+  const { data: products, error } = useProducts({
+    name_like: debouncedSearch,
+  });
+
+  if (error) {
+    return (
+      <Box p={2}>
+        <Typography variant="h4" gutterBottom>
+          Error
+        </Typography>
+        <Typography>
+          Ha ocurrido un error al cargar los productos.
+        </Typography>
+      </Box>
+    );
+  }
 
   return (
     <Box p={2}>
@@ -24,6 +45,22 @@ export default function Products() {
       <Typography>
         Resultados para: {search || "todos los productos"}
       </Typography>
+
+      {
+        products?.data?.map((product: Product) => (
+          <>
+            <Box key={product.id}>  
+              <Typography>{product.name}</Typography>
+              <Typography>{product.price}</Typography>
+              <Typography>{product.stock}</Typography>
+              <Typography>{product.category?.name}</Typography>
+              <Typography>{product.subcategory?.name}</Typography>
+              <Typography>{product.brand?.name}</Typography>
+            </Box>
+            <br />
+          </>
+        ))
+      }
     </Box>
   );
 }
